036ea98701658c062ea7e21c954eb16218830758
Gregor <ghall@rundl.com>
2020-09-02 22:33:42 +1000

    background

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
+    <img src="/elodie.JPG" id="bg" alt="" style={{position:"fixed", top:0, left:0, opacity:0.15, minWidth: "100%", minHeight: "100%"}}></img>
     <App />
   </ThemeProvider>,
   document.querySelector('#root'),
